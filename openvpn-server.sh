996c7d4292f40878b3ead55e974725968b6d6624
MonPham <demondhunter93@gmail.com>
2023-02-27 16:15:18 +0700

    .

--- openvpn-server.sh
+++ openvpn-server.sh
@@ -7,7 +7,11 @@ echo "listen  muon
 systemctl restart haproxy.service
 OVPN_DATA="ovpn-data-example"
 PULIC_IP=$(curl https://ipinfo.io/ip)
+
+apt update && apt install docker -y
+
 docker volume create --name $OVPN_DATA
+
 docker run -v $OVPN_DATA:/etc/openvpn --rm kylemanna/openvpn ovpn_genconfig -u "udp://$PULIC_IP"
 docker run -v $OVPN_DATA:/etc/openvpn --rm -it kylemanna/openvpn ovpn_initpki
 docker run -v $OVPN_DATA:/etc/openvpn -d -p 1194:1194/udp --cap-add=NET_ADMIN kylemanna/openvpn
