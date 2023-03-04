57374e7884062a9465e568295e92ede53d646d3a
MonPham <demondhunter93@gmail.com>
2023-03-04 15:17:31 +0700

    .

--- option-vpn/script.sh
+++ option-vpn/script.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-config_file=$(find /openvpn -name '*.conf' -o -name '*.ovpn' 2> /dev/null | sort | shuf -n 1)
+config_file=$(find /openvpn -name '*.ovpn' 2> /dev/null | sort | shuf -n 1)
 if [[ -z $config_file ]]; then
     echo "no openvpn configuration file found" >&2
     exit 1
