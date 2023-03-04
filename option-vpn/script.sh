f23405964c5824e6a4f44b08045a7e285db69659
MonPham <demondhunter93@gmail.com>
2023-03-04 15:42:55 +0700

    .

--- option-vpn/script.sh
+++ option-vpn/script.sh
@@ -9,11 +9,12 @@ echo "run with config file $config_file"
 # ip tuntap show
 # modprobe tun
 # echo "end show info"
-openvpn --config "$config_file" --dev tun --redirect-gateway 'bypass-dns' 
+openvpn --config "$config_file" --dev tun --redirect-gateway &
+openvpn_pid=$!
 node testnet-generate-env.js 
 service cron start
 pm2 start ecosystem.config.cjs 
 service redis-server start && mongod --fork -f /etc/mongod.conf
 echo "nameserver 8.8.8.8" >> /etc/resolv.conf
 
-pm2 logs
+wait $openvpn_pid
