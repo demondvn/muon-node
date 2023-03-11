8f3a3a451f7bf4e1cff1430b019f274f5e08fedc
MonPham <demondhunter93@gmail.com>
2023-03-11 22:25:35 +0700

    .

--- installer.sh
+++ installer.sh
@@ -1,70 +1,111 @@
 #!/bin/bash
 
-# Prompt the user for remote IP and password
-read -p "Enter remote IP: " REMOTE
-read -s -p "Enter password: " PASS
-apt install sshpass -y
-# SSH into the remote host and run Docker container
-sshpass -p '$PASS' ssh root@$REMOTE "sudo docker run -d \
-       --name wg-easy \
-       -e WG_HOST=$(curl https://ipinfo.io/ip) \
-       -e PASSWORD=P@ssw0rd \
-       -v $(pwd):/etc/wireguard \
-       -p 51820:51820/udp \
-       -p 51821:51821/tcp \
-       -p 4000:4000 \
-       -p 8000:8000 \
-       --cap-add=NET_ADMIN \
-       --cap-add=SYS_MODULE \
-       --sysctl net.ipv4.ip_forward=1 \
-       --sysctl net.ipv4.conf.all.src_valid_mark=1 \
-       weejewel/wg-easy"
-
-
-
-
-# Print instructions for the user
-echo "User login http://$REMOTE:51821 with P@ssw0rd"
-echo "Create client 'wg0' and download the setting file"
-sleep 60
-echo "Save the file into client folder: 'root/vpn/1'"
-
-# Prompt the user for the config folder
-read -p "Enter the VPN config folder: " CONFIG
-
-# Check if wg0.conf file exists
-if [ ! -f "$CONFIG/wg0.conf" ]; then
-  echo "Error: file wg0.conf not found"
-  exit 1
+# Install sshpass if not installed
+if ! command -v sshpass &> /dev/null
+then
+    sudo apt-get install sshpass -y
 fi
-echi "Clone "
-git clone https://github.com/demondvn/muon-node.git
-cd muon-node
-echo "# Build Docker File #"
-echo "#####################"
-cd wireguard
-docker build . -t muon --pull
-# Prompt the user for the Docker sequence number
-read -p "Enter the Docker sequence number: " SEQUENCE
 
-# Run the Docker container
-docker run -d \
-       --name=muon-$SEQUENCE \
-       --cap-add=NET_ADMIN \
-       --cap-add=SYS_MODULE \
-       -e PUID=1000 \
-       -e PGID=1000 \
-       --dns 8.8.8.8 \
-       -v $CONFIG:/config \
-       -v /lib/modules:/lib/modules \
-       --sysctl net.ipv4.conf.all.src_valid_mark=1 \
-       --restart=unless-stopped \
-       muon
+# Read FILE argument with data
+FILE="$1"
 
-# Get the IP address of the Docker container
-IP_ADDRESS=$(sudo docker exec muon-$SEQUENCE ip a | grep 'inet 10.8.0' | awk '{print $2}' | cut -d'/' -f1)
+# Read data from FILE and execute the commands for each row
+while read -r ROW
+do
+    # Extract REMOTE, PASS, CONFIG and SEQUENCE from the row
+    REMOTE=$(echo "$ROW" | awk '{print $2}')
+    PASS=$(echo "$ROW" | awk '{print $3}')
+    CONFIG=$(echo "$ROW" | awk '{print $4}')
+    SEQUENCE=$(echo "$ROW" | awk '{print $1}')
 
-# SSH into the remote host and perform some configuration steps
-ssh root@$REMOTE "echo $PASS | sudo -S docker exec wg-easy iptables -t nat -A PREROUTING -p tcp --dport 4000 -j DNAT --to-destination $IP_ADDRESS"
-ssh root@$REMOTE "echo $PASS | sudo -S docker exec wg-easy iptables -t nat -A PREROUTING -p tcp --dport 8000 -j DNAT --to-destination $IP_ADDRESS"
-curl $REMOTE:8000/status
+    echo "Building Docker for VPS $REMOTE ..."
+
+    # Install Docker on remote server and run WireGuard container
+    sshpass -p "$PASS" ssh root@"$REMOTE" "apt-get update && \
+        apt-get install docker.io -y && \
+        docker run -d --name wg-easy \
+            -e WG_HOST=$(curl https://ipinfo.io/ip) \
+            -e PASSWORD=P@ssw0rd \
+            -v $(pwd):/etc/wireguard \
+            -p 51820:51820/udp \
+            -p 51821:51821/tcp \
+            -p 4000:4000 \
+            -p 8000:8000 \
+            --cap-add=NET_ADMIN \
+            --cap-add=SYS_MODULE \
+            --sysctl net.ipv4.ip_forward=1 \
+            --sysctl net.ipv4.conf.all.src_valid_mark=1 \
+            weejewel/wg-easy"
+
+    # Function to get client ID
+    get_client_id() {
+        local name="wg0"
+
+        curl -s -X POST \
+            -H "Content-Type: application/json" \
+            -d '{"password":"P@ssw0rd"}' \
+            -c "$REMOTE.txt" \
+            "http://$REMOTE:51821/api/session"
+
+        curl -s -X POST \
+            -H "Content-Type: application/json" \
+            -d "{\"name\":\"$name\"}" \
+            -b "$REMOTE.txt" \
+            "http://$REMOTE:51821/api/wireguard/client"
+
+        local url="http://$REMOTE:51821/api/wireguard/client"
+        local response=$(curl -s "$url")
+        local id=$(echo "$response" | jq -r ".[] | select(.name == \"$name\") | .id")
+
+        echo "$id"
+    }
+
+    # Function to download WireGuard configuration
+    download_config() {
+        local dir="$1"
+        local id=$(get_client_id)
+        local url="http://$REMOTE:51821/api/wireguard/client/$id/configuration"
+        local filename="$dir/wg0.conf"
+
+        curl -s "$url" -o "$filename"
+    }
+
+    # Download WireGuard configuration
+    download_config "$CONFIG"
+
+    # Check if the config file exists
+    if [ ! -f "$CONFIG/wg0.conf" ]; then
+        echo "Error: File $CONFIG/wg0.conf not found"
+        exit 1
+    fi
+
+    # Build Docker image for WireGuard client
+    cd wireguard
+    docker build . -t muon --pull
+
+    # Run Docker container for WireGuard client
+    docker run -d \
+    --name=muon-"$SEQUENCE" \
+    --cap-add=NET_ADMIN \
+    --cap-add=SYS_MODULE \
+    -e PUID=1000 \
+    -e PGID=1000 \
+    --dns 8.8.8.8 \
+    -v /root/vpn/1:/config \
+    -v /lib/modules:/lib/modules \
+    --sysctl net.ipv4.conf.all.src_valid_mark=1 \
+    --restart=unless-stopped \
+    muon
+
+ # Check IP address of client container
+  ip_address=$(sudo docker exec muon-$SEQUENCE ip a | grep 'inet 10.8.0' | awk '{print $2}' | cut -d'/' -f1)
+  echo "Client IP address: $ip_address"
+
+  # Set NAT from VPS to client container
+  sshpass -p "$PASS" ssh root@"$REMOTE" \
+    "sudo docker exec wg-easy iptables -t nat -A PREROUTING -p tcp --dport 4000 -j DNAT --to-destination $ip_address && \
+     sudo docker exec wg-easy iptables -t nat -A PREROUTING -p tcp --dport 8000 -j DNAT --to-destination $ip_address"
+  
+  # Print status
+  curl "$REMOTE":8000/status
+done < "$FILE"
