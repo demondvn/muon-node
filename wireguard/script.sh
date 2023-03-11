18ca562fe8cab656196cdeccf6950f8ebb24090b
MonPham <demondhunter93@gmail.com>
2023-03-11 09:21:37 +0700

    .

--- wireguard/script.sh
+++ wireguard/script.sh
@@ -4,9 +4,8 @@ ls
 cd /app
 node testnet-generate-env.js 
 service cron start
-pm2 start ecosystem.config.cjs 
 service redis-server start &
 mongod --fork -f /etc/mongod.conf 
-pm2 log
+pm2 start ecosystem.config.cjs --no-daemon
 
 
