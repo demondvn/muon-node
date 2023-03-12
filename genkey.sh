81e1105cafbad35bae43be9b3b3e97d5855e2ecd
MonPham <demondhunter93@gmail.com>
2023-03-12 16:23:49 +0700

    .

--- genkey.sh
+++ genkey.sh
@@ -1,18 +1,41 @@
 #!/bin/bash
-# Generate client private key
-    wg genkey > client_private.key
+apt-get update && \
+apt-get install docker.io wget -y && \
+docker run -d --name wg-easy \
+    -e WG_HOST=$(curl https://ipinfo.io/ip) \
+    -e PASSWORD=$1 \
+    -v $(pwd):/etc/wireguard \
+    -p 51820:51820/udp \
+    -p 51821:51821/tcp \
+    -p 4000:4000 \
+    -p 8000:8000 \
+    --cap-add=NET_ADMIN \
+    --cap-add=SYS_MODULE \
+    --sysctl net.ipv4.ip_forward=1 \
+    --sysctl net.ipv4.conf.all.src_valid_mark=1 \
+    weejewel/wg-easy
 
-    # Generate client public key from private key
-    wg pubkey < client_private.key > client_public.key
+docker exec wg-easy bash -c 'umask  077' 
+docker exec wg-easy bash -c 'wg genkey > client_private.key'  
+docker exec wg-easy bash -c 'wg pubkey < client_private.key >client_public.key'  
+docker exec wg-easy bash -c 'wg genpsk > client_share.key'  
+docker exec wg-easy bash -c 'echo "
+[Interface]
+PrivateKey = $(cat client_private.key)
+Address = 10.8.0.2/24
+DNS = 1.1.1.1
 
-    # Create client config file
-    echo "[Interface]" > wg0.conf
-    echo "PrivateKey = $$(cat client_private.key)" >> wg0.conf
-    echo "Address = 10.8.0.2/24" >> wg0.conf
-    echo "DNS = 1.1.1.1" >> wg0.conf
-    echo "" >> wg0.conf
-    echo "[Peer]" >> wg0.conf
-    echo "PublicKey = $$(cat client_public.key)" >> wg0.conf
-    echo "AllowedIPs = 0.0.0.0/0" >> wg0.conf
-    echo "PersistentKeepalive = 0" >> wg0.conf
-    echo "Endpoint = $$(curl https://ipinfo.io/ip):51820" >> wg0.conf
+
+[Peer]
+PublicKey = $(cat client_public.key)
+PresharedKey =  $(cat client_share.key)
+AllowedIPs = 0.0.0.0/0
+PersistentKeepalive = 0
+" > client.conf'
+docker cp wg-easy:/app/client.conf client.conf
+echo "Endpoint = $(curl https://ipinfo.io/ip):51820" >> client.conf
+
+docker exec wg-easy iptables -t nat -A PREROUTING -p tcp --dport 4000 -j DNAT --to-destination 10.8.0.2
+docker exec wg-easy iptables -t nat -A PREROUTING -p tcp --dport 8000 -j DNAT --to-destination 10.8.0.2
+
+
---
+++ install_vps.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+
+# Install sshpass if not installed
+if ! command -v sshpass &> /dev/null
+then
+    sudo apt-get install sshpass -y
+fi
+
+# Read FILE argument with data
+FILE="$1"
+if ! "$FILE" 
+then 
+    echo "missing file vps"
+fi
+read -p "Input Wireguard password:" WIREGUARD_PASS
+# Read data from FILE and execute the commands for each row
+while read -r ROW
+do
+    # Extract REMOTE, PASS, CONFIG and SEQUENCE from the row
+    REMOTE=$(echo "$ROW" | awk '{print $1}')
+    USER=$(echo "$ROW" | awk '{print $2}')
+    PASS=$(echo "$ROW" | awk '{print $3}')
+
+    mkdir "$CONFIG"
+    echo "Building Docker for VPS $REMOTE | $USER ..."
+
+    # Install Docker on remote server and run WireGuard container
+    sshpass -p "$PASS" ssh "$USER"@"$REMOTE" "apt update && apt install wget -y && \
+    wget https://raw.githubusercontent.com/demondvn/muon-node/main/genkey.sh && \
+    chmod +x genkey.sh && ./genkey.sh $WIREGUARD_PASS
+    "
+
+    CONFIG = "vpn/$REMOTE"
+
+    mkdir "$CONFIG"
+
+    # Generate client private key
+
+
+    sshpass -p "$PASS" scp "$USER"@"$REMOTE":/root/client.conf "$CONFIG/wg0.conf"
+    
+
+    
+
+    # Check if the config file exists
+    if [  -f "$CONFIG/wg0.conf" ]; then
+        # Build Docker image for WireGuard client
+        # git clone https://github.com/demondvn/muon-node.git
+        cd wireguard
+        docker build . -t muon --pull
+
+        # Run Docker container for WireGuard client
+        docker run -d \
+        --name=muon-"$SEQUENCE" \
+        --cap-add=NET_ADMIN \
+        --cap-add=SYS_MODULE \
+        -e PUID=1000 \
+        -e PGID=1000 \
+        --dns 8.8.8.8 \
+        -v "$CONFIG":/config \
+        -v /lib/modules:/lib/modules \
+        --sysctl net.ipv4.conf.all.src_valid_mark=1 \
+        --restart=unless-stopped \
+        muon
+
+        # Print status
+        curl "$REMOTE":8000/status
+        
+    fi
+    echo "Error: File $CONFIG/wg0.conf not found"
+   
+done < "$FILE"
