5e6328c395a7479a3e287036b005d2ee54e51708
MonPham <demondhunter93@gmail.com>
2023-03-13 11:34:11 +0700

    Update genkey.sh

--- genkey.sh
+++ genkey.sh
@@ -15,26 +15,6 @@ docker run -d --name wg-easy \
     --sysctl net.ipv4.conf.all.src_valid_mark=1 \
     weejewel/wg-easy
 
-docker exec wg-easy bash -c 'umask  077' 
-docker exec wg-easy bash -c 'wg genkey > client_private.key'  
-docker exec wg-easy bash -c 'wg pubkey < client_private.key >client_public.key'  
-docker exec wg-easy bash -c 'wg genpsk > client_share.key'  
-docker exec wg-easy bash -c 'echo "
-[Interface]
-PrivateKey = $(cat client_private.key)
-Address = 10.8.0.2/24
-DNS = 1.1.1.1
-
-
-[Peer]
-PublicKey = $(cat client_public.key)
-PresharedKey =  $(cat client_share.key)
-AllowedIPs = 0.0.0.0/0
-PersistentKeepalive = 0
-" > client.conf'
-docker cp wg-easy:/app/client.conf client.conf
-echo "Endpoint = $(curl -q https://ipinfo.io/ip):51820" >> client.conf
-
 docker exec wg-easy iptables -t nat -A PREROUTING -p tcp --dport 4000 -j DNAT --to-destination 10.8.0.2
 docker exec wg-easy iptables -t nat -A PREROUTING -p tcp --dport 8000 -j DNAT --to-destination 10.8.0.2
 
