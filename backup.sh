a51a507b8328068c90486d1cebd8e2c90dcea52b
MonPham <demondhunter93@gmail.com>
2023-03-26 09:50:42 +0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -8,7 +8,7 @@ for name in $(docker ps --format '{{.Names}}'); do
 
     # Copy .env file to /config/.env inside the container
     #docker exec $name cp .env /config/.env
-        docker cp $name:/usr/src/muon-node-js/.env backup/$name.env
+        docker cp $name:/usr/src/muon-node-js/.env backup/$name/.env
   fi
 
 done
