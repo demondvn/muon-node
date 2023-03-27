b2214a2a68c645bb534cb11c67e5faf4c76feed4
MonPham <demondhunter93@gmail.com>
2023-03-27 16:02:34 +0700

    .

--- fix_update.sh
+++ fix_update.sh
@@ -4,7 +4,8 @@ for name in $(docker ps --format '{{.Names}}'); do
 
   # Check if the container name starts with "muon"
   if [[ $name == muon* ]]; then
-        docker exec $name git remote set-url origin https://github.com/muon-protocol/muon-node-js.git
+        docker exec $name git remote set-url origin https://github.com/muon-protocol/muon-node-js.git && git pull
+        
         docker exec $name ./scripts/auto-update.sh -a update
   fi
 
